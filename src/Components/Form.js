026b5a0cc63e57eb8aa8738bdd86647c338040c5
Meralle <mar.ha17@outlook.com>
2018-05-15 15:48:18 +0200

    styled input

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,20 +3,16 @@ class Form extends Component {
   render() {
     return (
       <div className="container">
-       <h1><strong>Mailchimp</strong></h1><br/><br/>
-	
-       
-    <form className="container row">
-      	<div className="row">
-      			
-	      	<div className="col s10">	 	
-	         	<input type="email" id="input" value={this.props.value} onChange={this.props.changeInput}/><br/>
-         	</div>
-         	<div className="col s2">
-	      		<button className="bttn" onClick={this.props.handleAddUser} type="button" id="button"  className=" red lighten-1 btn  btn-small right"><i className="material-icons">send</i>submit</button>
-	      	</div>
-        </div>
-    </form>
+       <h1><strong>Mailchimp</strong></h1><br/><br/>    
+	    <form className="container">
+	    	<div className="row">
+	      	<div className="input-field inline s8">		 	
+		    	<input type="email" id="input" value={this.props.value} onChange={this.props.changeInput}/>	
+	        </div>
+
+	        <button onClick={this.props.handleAddUser} type="button" id="button"  className=" red lighten-1 btn  btn-small "><i className="material-icons">send</i>submit</button>		
+	       </div>
+	    </form>
 
       </div>
     );
