1527d80a0f9285101427311f4fa86bd0b2e1b21f
Meralle <mar.ha17@outlook.com>
2018-05-16 12:03:40 +0200

    installed dotenv + create folder .env + addes if else statements for success and error

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -8,58 +9,57 @@ class App extends Component {
   state={
     result:"",
     username:"",
-    response:""
+    
     
   }
 
   changeInput = (e) => {
     this.setState({ username: e.target.value })   
- }
+}
 
 
- handleAddUser = (e) => {
+  handleAddUser = (e) => {
     let email = this.state.username
-    fetch(`https://cors-anywhere.herokuapp.com/https://us18.api.mailchimp.com/2.0/lists/subscribe.json?apikey=e3e51ca43f687f0ebbabeac29056f7cc-us18&id=074496cd86&email[email]=${email}&double_optin=true&send_welcome=false`)
+    let apiKey = process.env.REACT_APP_APIKEY
+    let id = process.env.REACT_APP_LISTID
+    fetch(`https://cors-anywhere.herokuapp.com/https://us18.api.mailchimp.com/2.0/lists/subscribe.json?apikey=${apiKey}&id=${id}&email[email]=${email}&double_optin=true&send_welcome=false`)
       .then(response => response.json())
       .then(result =>
 
         {this.setState({ result, username:""})
          console.log(result)
         if(result.email !== undefined){
-          this.setState({username: "success"})
-        } else if (result.email === "list_Allready subscribed"){
-          this.setState({username: "Already subscribed"})
-        }
-       
-        
-  })
+          this.setState({ username: "Successfully done,AlreadySubscribed" })
+        } else if (result.name){
+          this.setState({ username: result.name })
+        }      
+    })
       .catch(err =>{
         console.error('Fetch error', err)
-  });
- }
+    });
+}
       
              
 
-  render() {
-    // let template = if(this.state.username === "success"){
-    //   return ()
-    // }
-
-
+  render() {  
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        
-        <Form value={this.state.username}
-              changeInput={this.changeInput}
-              username={this.state.username}
-              handleAddUser={()=>{this.handleAddUser()} }
-               />
-          <div><p>{this.state.result.email}
-                    {this.state.username}</p></div>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            <p className="App-intero">
+             
+            </p>
+          </header>
+          <Form value={this.state.username}
+                changeInput={this.changeInput}
+                username={this.state.username}
+                handleAddUser={()=>{this.handleAddUser()} }
+                 />
+          <div>
+            <p>{this.state.result.email}</p>
+          {/*  <p>{this.state.username}</p>*/}
+          </div>           
       </div>
     );
   }
